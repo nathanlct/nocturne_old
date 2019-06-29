6efbaa4832f929e23c98dc98afb39a0dbac7f516
Fangyu Wu <fywu85@gmail.com>
2019-06-28 18:18:21 -0700

    Refactorize for cmake.

---
+++ build/CMakeFiles/nocturne.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/fangyu/GitHub/nocturne/src/Car.cpp" "/home/fangyu/GitHub/nocturne/build/CMakeFiles/nocturne.dir/src/Car.cpp.o"
+  "/home/fangyu/GitHub/nocturne/src/main.cpp" "/home/fangyu/GitHub/nocturne/build/CMakeFiles/nocturne.dir/src/main.cpp.o"
+  "/home/fangyu/GitHub/nocturne/src/utils.cpp" "/home/fangyu/GitHub/nocturne/build/CMakeFiles/nocturne.dir/src/utils.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/fangyu/GitHub/Box2D"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
